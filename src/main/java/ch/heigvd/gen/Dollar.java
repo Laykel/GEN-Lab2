8c05593d47b8e8aae2c0eff234ac7af825a7aac6
Laykel <layke.luc@gmail.com>
2019-03-22 14:45:25 +0100

    20.2, #2

--- src/main/java/ch/heigvd/gen/Dollar.java
+++ src/main/java/ch/heigvd/gen/Dollar.java
@@ -4,13 +4,13 @@ package ch.heigvd.gen;
  * Dollar class
  */
 public class Dollar {
-    int amount = 5 * 2;
+    int amount;
 
     Dollar(int amount) {
 
     }
 
     void times(int multiplier) {
-
+        amount = 5 * 2;
     }
 }
