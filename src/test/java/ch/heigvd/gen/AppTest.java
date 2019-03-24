1386cbab6d38d193f013fbd525ef7e2fa719158b
Laykel <layke.luc@gmail.com>
2019-03-24 16:57:46 +0100

    25.2, closes #6

--- src/test/java/ch/heigvd/gen/AppTest.java
+++ src/test/java/ch/heigvd/gen/AppTest.java
@@ -2,6 +2,7 @@ package ch.heigvd.gen;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -27,5 +28,6 @@ public class AppTest {
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
