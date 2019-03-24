4de184d4f8c3b96bb02aa27a40a01598e6d3a392
Laykel <layke.luc@gmail.com>
2019-03-24 17:44:38 +0100

    33.4, #12

--- src/main/java/ch/heigvd/gen/Dollar.java
+++ src/main/java/ch/heigvd/gen/Dollar.java
@@ -20,15 +20,4 @@ public class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-
-    /**
-     * Equality method for Money
-     * @param object The object to be equal to
-     * @return true if the two objects are equal, false else
-     */
-    @Override
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount;
-    }
 }
--- src/main/java/ch/heigvd/gen/Money.java
+++ src/main/java/ch/heigvd/gen/Money.java
@@ -5,4 +5,15 @@ package ch.heigvd.gen;
  */
 public class Money {
     protected int amount;
+
+    /**
+     * Equality method for Money
+     * @param object The object to be equal to
+     * @return true if the two objects are equal, false else
+     */
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
