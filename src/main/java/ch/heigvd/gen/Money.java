0346bc6e15f90cfab5c5264fc6667d8a667dd3ee
Laykel <layke.luc@gmail.com>
2019-03-25 16:34:51 +0100

    37.2, #13

--- src/main/java/ch/heigvd/gen/Money.java
+++ src/main/java/ch/heigvd/gen/Money.java
@@ -7,6 +7,15 @@ public class Money {
     protected int amount;
 
     /**
+     * Factory for dollar objects
+     * @param amount
+     * @return
+     */
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    /**
      * Equality method for Money
      * @param object The object to be equal to
      * @return true if the two objects are equal, false else
--- src/test/java/ch/heigvd/gen/AppTest.java
+++ src/test/java/ch/heigvd/gen/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest {
      */
     @Test
     public void testDollarMultiplication() {
-        Dollar five = new Dollar(5);
+        Dollar five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
