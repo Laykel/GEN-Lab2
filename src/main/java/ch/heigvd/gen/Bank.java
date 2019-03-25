9c60e9c7107f76016053857d0fc948f26e66a3c0
Laykel <layke.luc@gmail.com>
2019-03-25 22:32:54 +0100

    59.1, fixes #19, closes #17

--- src/main/java/ch/heigvd/gen/Bank.java
+++ src/main/java/ch/heigvd/gen/Bank.java
@@ -2,9 +2,6 @@ package ch.heigvd.gen;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        if (source instanceof Money)
-            return (Money) source.reduce(to);
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
