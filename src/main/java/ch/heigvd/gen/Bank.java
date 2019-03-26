87ec2cb4c7feb419723316502630b7c5185f36d2
Laykel <layke.luc@gmail.com>
2019-03-26 22:38:29 +0100

    63, fixes #20

--- src/main/java/ch/heigvd/gen/Bank.java
+++ src/main/java/ch/heigvd/gen/Bank.java
@@ -31,9 +31,9 @@ public class Bank {
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") & to.equals("USD"))
-                ? 2
-                : 1;
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 
     void addRate(String from, String to, int rate) {
